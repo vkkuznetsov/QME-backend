e5f959614114ab12e735e216b952e5ed6f1098b9
Lerokkkk <vailyinykh@gmail.com>
2025-01-20 00:56:12 +0500

    start work with dishka

--- backend/api/healthcheck/healthcheck.py
+++ backend/api/healthcheck/healthcheck.py
@@ -1,11 +1,13 @@
+from dishka.integrations.fastapi import FromDishka, inject
 from fastapi import APIRouter
 
-from backend.logic.services.healthcheck.alchemy import PostgresHealthcheckService
+from backend.logic.services.healthcheck.base import IHealthCheckService
 
 router = APIRouter(tags=['healthcheks'], prefix='/healthcheck')
 
 
 @router.get('')
-async def database_healthcheck():
-    response_data = await PostgresHealthcheckService().check()
+@inject
+async def database_healthcheck(healthcheck_service: FromDishka[IHealthCheckService]):
+    response_data = await healthcheck_service.check()
     return response_data
