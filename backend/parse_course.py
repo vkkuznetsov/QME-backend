0c2607571eeaad2317471ea76799c974a11dfae5
vik <fake@email.com>
2025-03-30 13:14:59 +0500

    update parsing schedule

--- backend/parse_course.py
+++ backend/parse_course.py
@@ -18,8 +18,10 @@ from backend.database.models.student import student_group
 
 from logging import getLogger
 from backend.utils.time_measure import time_log
+
 name = __name__
 log = getLogger(name)
+
 
 class ElectiveFileParser:
     def __init__(self, file: UploadFile):
@@ -30,47 +32,19 @@ class ElectiveFileParser:
 
     async def __call__(self):
         await self.read_file()
-        await self.read_file_w()
+        await self.parse_file()
 
+    @time_log(name)
     async def read_file(self):
-        self.raw_df = pd.read_excel(self.file.file)
-        print(self.raw_df)
+        excel_file = pd.ExcelFile(self.file.file)
+        schedule_df = excel_file.parse("Расписание")
+        self.filtered_df = schedule_df
 
     @time_log(name)
     @db_session
-    async def read_file_w(self, db: AsyncSession):
-        try:
-            excel_file = pd.ExcelFile(self.file.file)
-
-            # Проверяем наличие листа "Расписание"
-            if "Расписание" not in excel_file.sheet_names:
-                print("Лист 'Расписание' не найден в файле.")
-                return None
-
-            # Загружаем лист "Расписание"
-            schedule_df = excel_file.parse("Расписание")
-            print("Содержимое листа 'Расписание' загружено.")
-
-            # Создаём словарь с группировкой по "РМУП название"
-            grouped_data = defaultdict(list)
-
-            for _, row in schedule_df.iterrows():
-                group = row["РМУП название"]
-                entry = {
-                    "Команда": row["Команда название"],
-                    "Сотрудники": row["Сотрудники в команде"],
-                    "Свободные места": row["свободных мест в команде"],
-                    "День недели": row["День недели"],
-                    "Время": row["Время проведения"]
-                }
-                grouped_data[group].append(entry)
-
-            print(f"Обработано {len(grouped_data)} групп.")
-
-
-            return grouped_data
-
-        except Exception as e:
-            print(f"Ошибка при чтении файла: {e}")
-            return None
-
+    async def parse_file(self, db: AsyncSession):
+        final_d = set()
+        for _, row in self.filtered_df.iterrows():
+            new_data = row['РМУП название'], row['Команда название'], row['свободных мест в команде'], row['Время проведения']
+            final_d.add(new_data)
+        print(final_d)
