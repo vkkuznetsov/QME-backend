d67fc55ece186616c0c9d61a2859eeef297d68a4
vikku <vita.201581@yandex.ru>
2025-06-06 14:18:47 +0500

    fixed import models

--- backend/database/models/__init__.py
+++ backend/database/models/__init__.py
@@ -1,4 +1,5 @@
 from backend.database.models.student import Student, student_group
+from backend.database.models.manager import Manager
 from backend.database.models.elective import Elective
 from backend.database.models.group import Group, Teacher, group_teacher
 from backend.database.models.journal import Journal
